dc51129fe7bc8fecc9933969ab65f5b37233f53f
Bruno dos Anjos Santos <brunoanjos21@gmail.com>
2026-09-07 19:59:36 -0300

    Finalização do basico

--- MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
@@ -1,3 +1,5 @@
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class EditarProduto : ContentPage
@@ -6,4 +8,30 @@ public partial class EditarProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        { //model montada
+            Produto produto_anexado = BindingContext as Produto;
+
+            Produto p = new Produto
+            {
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text),
+            };
+
+            await App.Db.Update(p); //insert pq esta fazendo um POST
+            await DisplayAlert("Sucesso,", "Registrado com Sucesso!", "Sucesso!");
+            await Navigation.PopAsync();
+
+
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+
+    }
 }
